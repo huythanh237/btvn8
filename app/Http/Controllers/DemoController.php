1db43e02f37fd69c7c2489539141855cdbd30dac
huythanh237 <huyxmen237@gmail.com>
2021-04-26 08:26:55 +0700

    First Commit

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePost;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::query()->orderByDesc('id')->paginate(10);
+
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePost $request)
+    {
+
+
+
+        $post = Post::query()->create($request->only('slug','title','content'));
+
+        if($file = $request->file('image')){
+            $filename = date('YmdHis') . $file->getClientOriginalName();
+            $file->move(public_path('images'), $filename);
+
+            $post->update(['image' => 'images' . '/' . $filename]);
+        }
+
+
+//        $post = Post::query()->create($request->only('slug','title','content'));
+//
+//        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::query()->findOrFail($id);
+
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::query()->findOrFail($id);
+
+        $post->update($request->only('slug','title','content'));
+
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Post::destroy($id);
+    }
+}
